26c76515cfd5078465d63acfede7a829dd5a2a35
tsesme01 <petra.tsesmetzi.ext@ptb.de>
2023-11-03 16:47:03 +0100

    controller ausgebaut findById, findAll, create

--- src/main/java/com/example/myfirstrestapp/Todo.java
+++ src/main/java/com/example/myfirstrestapp/Todo.java
@@ -1,10 +1,14 @@
 package com.example.myfirstrestapp;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-
+@Entity//damit sagen wir  hybernate erstelle  mir eine Tabelle
 public class Todo {
-
+    @Id//das setzt id auf Primarykey
+    @GeneratedValue(strategy = GenerationType.AUTO)//autoincrement +1 des primary keys
     private Integer id;
     private String description;
     private boolean isDone;
--- src/main/java/com/example/myfirstrestapp/TodoController.java
+++ src/main/java/com/example/myfirstrestapp/TodoController.java
@@ -1,22 +1,39 @@
 package com.example.myfirstrestapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 public class TodoController {
-   @GetMapping("/todo")
-    public ResponseEntity<String> get(@RequestParam(value="id")int id){
-        Todo newTodo=new Todo();
-         newTodo.setId(id);
-         newTodo.setDescription("Einkaufen");
-         newTodo.setIsDone(true);
-       return new ResponseEntity(newTodo, HttpStatus.OK);
+
+    @Autowired//Field Dependency-Injection damit injiziert spring intern eine Objekt
+    private TodoRepository todoRepository;
+
+    @GetMapping("/todo")
+    public ResponseEntity<String> get(@RequestParam(value = "id") int id) {
+
+        Optional<Todo> todoInDb = todoRepository.findById(id);
+        if(todoInDb.isPresent()){
+            return new ResponseEntity(todoInDb.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity("No Todo found with id "+id, HttpStatus.NOT_FOUND);
+
     }
+
+    @GetMapping("/todo/all")
+    public ResponseEntity<Iterable<Todo>>getAll(){
+        Iterable<Todo>allTodosInDb=todoRepository.findAll();
+        return new ResponseEntity<Iterable<Todo>>(allTodosInDb,HttpStatus.OK);
+    }
+
     @PostMapping("/todo")
-    public ResponseEntity<Todo>create(@RequestBody Todo newTodo){
-       //save to db
+    public ResponseEntity<Todo> create(@RequestBody Todo newTodo) {
+        //save to db
+        todoRepository.save(newTodo);
         return new ResponseEntity<Todo>(newTodo, HttpStatus.OK);
 
     }
